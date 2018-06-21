baef216333bb7c9ed6c567ca47469e13b310b275
piyush27ranjan <piyush.27ranjan@gmail.com>
2018-06-21 19:11:45 +0530

    run test on sample.png

--- 1shot_test.py
+++ 1shot_test.py
@@ -3,10 +3,12 @@ import numpy as np
 
 # Coordinates of background
 # background = {'top': 334, 'left': 26, 'width': 1868, 'height': 620}
-filename = '1.png'
+filename = 'Sample_image.png'
 # Loads an image
 src = cv2.imread(filename)
 nsrc = src[334:334+620 , 26:26+1868]
+bottom_bar = src[962:962+50  , 23:23+1860]
+bottom_bar_gray = cv2.cvtColor(bottom_bar,cv2.COLOR_BGR2GRAY)
 gray = cv2.cvtColor(nsrc, cv2.COLOR_BGR2GRAY)
 gray = cv2.medianBlur(gray, 5)   
 rows = gray.shape[0]
@@ -26,7 +28,13 @@ if circles is not None:
             print("x:"+ str(i[0]) + " y:" + str(i[1]) + " r:" + str(i[2]))
             cv2.circle(nsrc, center, radius, (255, 0, 255), 3)
             print()
-
+ret, thresh = cv2.threshold(bottom_bar_gray, 60, 255, 0)
+contours = cv2.findContours(
+                thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnt = contours[0]
+x, y, w, h = cv2.boundingRect(cnt)
+caught_you = cv2.rectangle(bottom_bar,(x,y),(x+w,y+h),(255,255,255),5)
+cv2.imshow('caught_you',caught_you)
 cv2.imshow("detected circles", nsrc)
 cv2.waitKey(0)
 
